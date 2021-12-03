2f0f39d98c26aaad9ae624ecb5c90f33f8428247
Zaperox <minichris502@gmail.com>
2021-12-03 15:55:29 +0000

    updated plugin build script

--- BuildPlugin.bat
+++ BuildPlugin.bat
@@ -4,7 +4,8 @@ E:/UE_4.26/Engine/Binaries/DotNET/UnrealBuildTool.exe  -Module=Emergence Emergen
 E:/UE_4.26/Engine/Binaries/DotNET/UnrealBuildTool.exe  -Module=EmergenceEditor EmergenceUEExampleEditor Win64 Development -Project="H:/emergence-plugin-unreal/EmergenceUEExample.uproject" "H:/emergence-plugin-unreal/EmergenceUEExample.uproject"  -IgnoreJunk
 E:/UE_4.26/Engine/Binaries/DotNET/UnrealBuildTool.exe  -Module=EmergenceToolbar EmergenceUEExampleEditor Win64 Development -Project="H:/emergence-plugin-unreal/EmergenceUEExample.uproject" "H:/emergence-plugin-unreal/EmergenceUEExample.uproject"  -IgnoreJunk
 rmdir D:\EmergenceBuild /s /q
-robocopy %~dp0plugins\Emergence D:\EmergenceBuild\Emergence /E /XD "%~dp0plugins\Emergence\Intermediate" /XD "%~dp0plugins\Emergence\Source\Emergence\Private" /XD "%~dp0plugins\Emergence\Source\EmergenceEditor\Private" /XD "%~dp0plugins\Emergence\Source\EmergenceToolbar\Private"
+robocopy %~dp0plugins\Emergence D:\EmergenceBuild\Emergence /E
 cd /d D:\EmergenceBuild\
 tar -a -c -f D:\EmergenceBuild\Emergence.zip Emergence
+explorer.exe D:\EmergenceBuild
 pause
