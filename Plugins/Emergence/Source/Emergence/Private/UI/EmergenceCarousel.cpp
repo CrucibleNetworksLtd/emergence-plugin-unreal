6b6239a685112dfde6ee34c6bb3a3aea173b05b3
Zaperox <minichris502@gmail.com>
2022-02-15 16:23:16 +0000

    Removed unneeded c++ class

--- Plugins/Emergence/Source/Emergence/Private/UI/EmergenceCarousel.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Crucible Networks Ltd 2022. All Rights Reserved.
-
-#include "UI/EmergenceCarousel.h"
-#include "UI/EmergenceCarouselItem.h"
-
-void UEmergenceCarousel::Init(TArray<UEmergenceCarouselItem*>& itemsParam)
-{
-	items = itemsParam;
-}
