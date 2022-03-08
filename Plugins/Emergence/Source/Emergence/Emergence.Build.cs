bd20b1a5267749641da42ac95ee9c8ec249a08d0
Zaperox <minichris502@gmail.com>
2022-03-08 17:13:14 +0000

    this is required for including the Emergence singleton apprently

--- Plugins/Emergence/Source/Emergence/Emergence.Build.cs
+++ Plugins/Emergence/Source/Emergence/Emergence.Build.cs
@@ -29,7 +29,8 @@ public class Emergence : ModuleRules
 				"Core",
 				"UMG",
 				"Sockets",
-				"Networking"
+				"Networking",
+				"JsonUtilities"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
