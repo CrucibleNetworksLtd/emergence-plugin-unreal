b9820a805dcf47e33e8ba18d7317a059a5957dad
Zaperox <minichris502@gmail.com>
2022-04-27 22:42:28 +0100

    Added tooltips to some of the plugin settings

--- Plugins/Emergence/Source/EmergenceEditor/Private/EmergencePluginSettings.h
+++ Plugins/Emergence/Source/EmergenceEditor/Private/EmergencePluginSettings.h
@@ -13,14 +13,17 @@ public:
 	UEmergencePluginSettings(const FObjectInitializer& ObjectInitializer);
 
 	UPROPERTY(config, EditAnywhere, Category = "Emergence Server", meta = (FilePathFilter = "Emergence Server (EmergenceEVMLocalServer.exe)|EmergenceEVMLocalServer.exe"))
+	//Used to set the location of a custom version of the EVM server. Leave blank for the default.
 	FFilePath CustomEmergenceServerLocation;
 
 	UPROPERTY(config, EditAnywhere, Category = "Emergence Server")
 	FString NodeURL;
 
 	UPROPERTY(config, EditAnywhere, Category = "Emergence Server", meta=(DisplayName = "Custom Game ID"))
+	//An optional Emergence custom Game ID, which allows you to upload new 2d avatars which can be selected from the persona creation screen in the overlay. Leave blank for the default.
 	FString GameID;
 
 	UPROPERTY(config, EditAnywhere, Category = "Emergence Server")
+	//This is the symbol which will be shown as the units for the user's account balance. This should match the token of the blockchain your game is set to connect to. By default this is "MATIC".
 	FString TokenSymbol = "MATIC";
 };
