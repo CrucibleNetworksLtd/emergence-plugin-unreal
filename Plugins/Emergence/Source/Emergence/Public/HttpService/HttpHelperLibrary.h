878b285c2e6ce24f4299da142325fa8cd5e55e4d
Zaperox <minichris502@gmail.com>
2022-06-17 18:43:56 +0100

    these were needed for the last two commits, whoops

--- Plugins/Emergence/Source/Emergence/Public/HttpService/HttpHelperLibrary.h
+++ Plugins/Emergence/Source/Emergence/Public/HttpService/HttpHelperLibrary.h
@@ -22,7 +22,9 @@ public:
 	inline static const FString DatabaseAPIPublic = TEXT("https://pfy3t4mqjb.execute-api.us-east-1.amazonaws.com/staging/");
 	inline static const FString DatabaseAPIPrivate = TEXT("https://57l0bi6g53.execute-api.us-east-1.amazonaws.com/staging/");
 	inline static const FString InventoryService = TEXT("https://dysaw5zhak.us-east-1.awsapprunner.com/InventoryService/");
+	inline static const FString InventoryServiceHost = TEXT("dysaw5zhak.us-east-1.awsapprunner.com");
 	inline static const FString AvatarService = TEXT("https://dysaw5zhak.us-east-1.awsapprunner.com/AvatarSystem/");
+	inline static const FString AvatarServiceHost = TEXT("dysaw5zhak.us-east-1.awsapprunner.com");
 
 	template<typename T>
 	inline static TSharedRef<IHttpRequest, ESPMode::ThreadSafe> ExecuteHttpRequest(T* FunctionBindObject, void(T::* FunctionBindFunction)(FHttpRequestPtr, FHttpResponsePtr, bool), const FString& URL, const FString& Verb = TEXT("GET"), const float& Timeout = 60.0F, const TArray<TPair<FString, FString>>& Headers = TArray<TPair<FString, FString>>(), const FString& Content = FString(), const bool ProcessRequestInstantly = true)
