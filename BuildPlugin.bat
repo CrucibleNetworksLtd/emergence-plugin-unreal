ef859e48f043e5cda71e385be442b22dd3e7af42
Zaperox <minichris502@gmail.com>
2022-10-11 06:29:20 +0100

    Updated build plugin script

--- BuildPlugin.bat
+++ BuildPlugin.bat
@@ -1,11 +1,2 @@
-rmdir %~dp0plugins\Emergence\Binaries /s /q
-rmdir %~dp0plugins\Emergence\Intermediate /s /q
-E:/UE_4.26/Engine/Binaries/DotNET/UnrealBuildTool.exe  -Module=Emergence EmergenceUEExampleEditor Win64 Development -Project="H:/emergence-plugin-unreal/EmergenceUEExample.uproject" "H:/emergence-plugin-unreal/EmergenceUEExample.uproject"  -IgnoreJunk
-E:/UE_4.26/Engine/Binaries/DotNET/UnrealBuildTool.exe  -Module=EmergenceEditor EmergenceUEExampleEditor Win64 Development -Project="H:/emergence-plugin-unreal/EmergenceUEExample.uproject" "H:/emergence-plugin-unreal/EmergenceUEExample.uproject"  -IgnoreJunk
-E:/UE_4.26/Engine/Binaries/DotNET/UnrealBuildTool.exe  -Module=EmergenceToolbar EmergenceUEExampleEditor Win64 Development -Project="H:/emergence-plugin-unreal/EmergenceUEExample.uproject" "H:/emergence-plugin-unreal/EmergenceUEExample.uproject"  -IgnoreJunk
-rmdir D:\EmergenceBuild /s /q
-robocopy %~dp0plugins\Emergence D:\EmergenceBuild\Emergence /E
-cd /d D:\EmergenceBuild\
-tar -a -c -f D:\EmergenceBuild\Emergence.zip Emergence
-explorer.exe D:\EmergenceBuild
+tar.exe -C  %~dp0\Plugins -a -cf %~dp0\PluginsClean.zip "VRM4U\Content" "VRM4U\Resources" "VRM4U\Source" "VRM4U\ThirdParty" "VRM4U\VRM4U.uplugin" "EmergenceVRMSupport\Resources" "EmergenceVRMSupport\Source" "EmergenceVRMSupport\EmergenceVRMSupport.uplugin" "Emergence\Content" "Emergence\EmergenceServer" "Emergence\Resources" "Emergence\Source" "Emergence\Emergence.uplugin"
 pause
