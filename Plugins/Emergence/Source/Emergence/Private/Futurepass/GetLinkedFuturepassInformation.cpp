a20c7eadd421b0a3e6a1ec2270a4cf25690fba51
Christopher Lee <minichris502@gmail.com>
2023-11-02 03:40:28 +0000

    Added GetLinkedFuturepassInformation node

---
+++ Plugins/Emergence/Source/Emergence/Private/Futurepass/GetLinkedFuturepassInformation.cpp
@@ -0,0 +1,80 @@
+// Copyright Crucible Networks Ltd 2023. All Rights Reserved.
+
+
+#include "Futurepass/GetLinkedFuturepassInformation.h"
+#include "HttpService/HttpHelperLibrary.h"
+#include "Interfaces/IHttpResponse.h"
+#include "Interfaces/IHttpRequest.h"
+
+UGetLinkedFuturepassInformation* UGetLinkedFuturepassInformation::GetLinkedFuturepassInformation(UObject* WorldContextObject, FString Address)
+{
+	UGetLinkedFuturepassInformation* BlueprintNode = NewObject<UGetLinkedFuturepassInformation>();
+	BlueprintNode->Address = Address;
+	BlueprintNode->WorldContextObject = WorldContextObject;
+	return BlueprintNode;
+}
+
+void UGetLinkedFuturepassInformation::Activate(){
+	FString URL = "https://4yzj264is3.execute-api.us-west-2.amazonaws.com/api/v1/linked-futurepass?eoa=";
+	URL = URL + "1:EVM:" + Address;
+	LinkedFuturepassRequest = UHttpHelperLibrary::ExecuteHttpRequest<UGetLinkedFuturepassInformation>(
+		this,
+		nullptr,
+		URL,
+		"GET",
+		60.0F,
+		{},
+		"", false);
+
+	LinkedFuturepassRequest->OnProcessRequestComplete().BindLambda([&](FHttpRequestPtr req, FHttpResponsePtr res, bool bSucceeded) {
+		EErrorCode StatusCode;
+		FJsonObject JsonObject = UErrorCodeFunctionLibrary::TryParseResponseAsJson(res, bSucceeded, StatusCode);
+		UE_LOG(LogEmergenceHttp, Display, TEXT("GetLinkedFuturepassInformation part 1: %s"), *res->GetContentAsString());
+		if (StatusCode == EErrorCode::EmergenceOk) {	
+			TSharedPtr<FJsonValue> JsonValue;
+			TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(res->GetContentAsString());
+			if (FJsonSerializer::Deserialize(Reader, JsonValue)) {
+				FString OwnedFuturepass;
+				if(JsonValue->AsObject()->TryGetStringField("ownedFuturepass", OwnedFuturepass)){
+					FString LinkedEOAURL = "https://4yzj264is3.execute-api.us-west-2.amazonaws.com/api/v1/linked-eoa?futurepass=" + OwnedFuturepass;
+					
+					LinkedEOARequest = UHttpHelperLibrary::ExecuteHttpRequest<UGetLinkedFuturepassInformation>(
+						this,
+						nullptr,
+						LinkedEOAURL,
+						"GET",
+						60.0F,
+						{},
+						"", false);
+					LinkedEOARequest->OnProcessRequestComplete().BindLambda([&](FHttpRequestPtr req, FHttpResponsePtr res, bool bSucceeded2) {
+						UE_LOG(LogEmergenceHttp, Display, TEXT("GetLinkedFuturepassInformation part 2: %s"), *res->GetContentAsString());
+						this->OnGetLinkedFuturepassInformationCompleted.Broadcast(FLinkedFuturepassInformationResponse(res->GetContentAsString()), StatusCode);
+						return;
+					});
+					LinkedEOARequest->ProcessRequest();
+					
+				}
+				else if (JsonValue->AsObject()->HasTypedField<EJson::Null>("ownedFuturepass")) {
+					//this happens when the owned futurepass doesn't exist. Leave the response blank, but give an error.
+					this->OnGetLinkedFuturepassInformationCompleted.Broadcast(FLinkedFuturepassInformationResponse(), EErrorCode::Forbidden);
+					return;
+				}
+			}
+			else {
+				//Json deserialize failed, override error message
+				this->OnGetLinkedFuturepassInformationCompleted.Broadcast(FLinkedFuturepassInformationResponse(), EErrorCode::EmergenceClientJsonParseFailed);
+				return;
+			}
+		}
+		else {
+			//A HTTP error happened, actually use status code
+			this->OnGetLinkedFuturepassInformationCompleted.Broadcast(FLinkedFuturepassInformationResponse(), StatusCode);
+			return;
+		}
+		});
+	LinkedFuturepassRequest->ProcessRequest();
+}
+
+void UGetLinkedFuturepassInformation::GetLinkedFuturepassInformation_HttpRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded){
+	
+}
---
+++ Plugins/Emergence/Source/Emergence/Public/Futurepass/GetLinkedFuturepassInformation.h
@@ -0,0 +1,110 @@
+// Copyright Crucible Networks Ltd 2023. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EmergenceAsyncSingleRequestBase.h"
+#include "JsonObjectConverter.h"
+#include "GetLinkedFuturepassInformation.generated.h"
+
+USTRUCT(BlueprintType)
+struct FLinkedFuturepassEOA
+{
+
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Emergence Internal|Futurepass Methods")
+	int proxyType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Emergence Internal|Futurepass Methods")
+	FString eoa;
+
+	FLinkedFuturepassEOA() {};
+
+	FLinkedFuturepassEOA(int _proxyType, FString _eoa) {
+
+		proxyType = _proxyType;
+		eoa = _eoa;
+	}
+
+	FLinkedFuturepassEOA(FString _json_) {
+		FLinkedFuturepassEOA _tmpLinkedFuturepassEOA;
+
+		FJsonObjectConverter::JsonObjectStringToUStruct<FLinkedFuturepassEOA>(
+			_json_,
+			&_tmpLinkedFuturepassEOA,
+			0, 0);
+
+		proxyType = _tmpLinkedFuturepassEOA.proxyType;
+		eoa = _tmpLinkedFuturepassEOA.eoa;
+	};
+
+};
+
+USTRUCT(BlueprintType)
+struct FLinkedFuturepassInformationResponse
+{
+
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Emergence Internal|Futurepass Methods")
+	FString futurepass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Emergence Internal|Futurepass Methods")
+	FString ownerEoa;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Emergence Internal|Futurepass Methods")
+	TArray<FLinkedFuturepassEOA> linkedEoas;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Emergence Internal|Futurepass Methods")
+	TArray<FLinkedFuturepassEOA> invalidEoas;
+
+	FLinkedFuturepassInformationResponse() {};
+
+	FLinkedFuturepassInformationResponse(FString _futurepass, FString _ownerEoa, TArray<FLinkedFuturepassEOA> _linkedEoas, TArray<FLinkedFuturepassEOA> _invalidEoas) {
+
+		futurepass = _futurepass;
+		ownerEoa = _ownerEoa;
+		linkedEoas = _linkedEoas;
+		invalidEoas = _invalidEoas;
+	}
+
+	FLinkedFuturepassInformationResponse(FString _json_) {
+		FLinkedFuturepassInformationResponse _tmpLinkedFuturepassInformationResponse;
+
+		FJsonObjectConverter::JsonObjectStringToUStruct<FLinkedFuturepassInformationResponse>(
+			_json_,
+			&_tmpLinkedFuturepassInformationResponse,
+			0, 0);
+
+		futurepass = _tmpLinkedFuturepassInformationResponse.futurepass;
+		ownerEoa = _tmpLinkedFuturepassInformationResponse.ownerEoa;
+		linkedEoas = _tmpLinkedFuturepassInformationResponse.linkedEoas;
+		invalidEoas = _tmpLinkedFuturepassInformationResponse.invalidEoas;
+	};
+
+};
+
+
+UCLASS()
+class EMERGENCE_API UGetLinkedFuturepassInformation : public UEmergenceAsyncSingleRequestBase
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject"), Category = "Emergence Internal|Futurepass Methods")
+	static UGetLinkedFuturepassInformation* GetLinkedFuturepassInformation(UObject* WorldContextObject, FString Address);
+
+	virtual void Activate() override;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnGetLinkedFuturepassInformationCompleted, FLinkedFuturepassInformationResponse, Response, EErrorCode, StatusCode);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGetLinkedFuturepassInformationCompleted OnGetLinkedFuturepassInformationCompleted;
+private:
+	void GetLinkedFuturepassInformation_HttpRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+
+	TSharedPtr<IHttpRequest, ESPMode::ThreadSafe> LinkedFuturepassRequest;
+	TSharedPtr<IHttpRequest, ESPMode::ThreadSafe> LinkedEOARequest;
+
+	FString Address;
+};
