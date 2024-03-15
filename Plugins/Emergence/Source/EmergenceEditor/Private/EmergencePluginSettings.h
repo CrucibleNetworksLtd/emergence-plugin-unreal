5fcaa76b2f8eddcb510026c657c61c5bae57e5f6
Christopher Lee <minichris502@gmail.com>
2024-03-15 17:09:49 +0000

    Futureverse now have their own enviroments

--- Plugins/Emergence/Source/EmergenceEditor/Private/EmergencePluginSettings.h
+++ Plugins/Emergence/Source/EmergenceEditor/Private/EmergencePluginSettings.h
@@ -24,11 +24,11 @@ public:
 
 	//Which Futureverse cloud environment should be communicated with when the game is built as "Debug", "Development" or "Test". Default is "Staging".
 	UPROPERTY(config, EditAnywhere, Category = "Futureverse", meta = (DisplayName = "Debug/Development/Test build Futureverse cloud environment"))
-	EEmergenceEnvironment FutureverseDevelopmentEnvironment = EEmergenceEnvironment::Staging;
+	EFutureverseEnvironment FutureverseDevelopmentEnvironment = EFutureverseEnvironment::Development;
 
 	//Which Futureverse cloud environment should be communicated with when the game is built as "Shipping". Default is "Production".
 	UPROPERTY(config, EditAnywhere, Category = "Futureverse", meta = (DisplayName = "Shipping build Futureverse cloud environment"))
-	EEmergenceEnvironment FutureverseShippingEnvironment = EEmergenceEnvironment::Production;
+	EFutureverseEnvironment FutureverseShippingEnvironment = EFutureverseEnvironment::Production;
 	
 	//For Crucible use only! Enable the development environment. Not to be confused with the development build of the game.
 	UPROPERTY(AdvancedDisplay, config, EditAnywhere, Category = "General", meta = (DisplayName = "[INTERNAL] Enable Development Environment"))
