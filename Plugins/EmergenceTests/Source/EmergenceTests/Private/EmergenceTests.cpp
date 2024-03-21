7ddcf41ab0ff892e0b1019c3ffba72139ba59111
Christopher Lee <minichris502@gmail.com>
2024-03-21 17:58:58 +0000

    Added C++ module to EmergenceTests

---
+++ Plugins/EmergenceTests/Source/EmergenceTests/Private/EmergenceTests.cpp
@@ -0,0 +1,18 @@
+// Copyright Crucible Networks Ltd 2023. All Rights Reserved.
+
+#include "EmergenceTests.h"
+
+void FEmergenceTestsModule::StartupModule()
+{
+
+}
+
+void FEmergenceTestsModule::ShutdownModule()
+{
+
+}
+
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEmergenceTestsModule, EmergenceTests)
