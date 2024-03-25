695184efc688668fe25ac689da82affce246c99b
Christopher Lee <minichris502@gmail.com>
2024-03-25 09:21:34 +0000

    Backported 74bba61, 7ddcf41, but only the non-futureverse parts

---
+++ Plugins/EmergenceTests/Source/EmergenceTests/EmergenceTests.Build.cs
@@ -0,0 +1,24 @@
+// Copyright Crucible Networks Ltd 2023. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class EmergenceTests : ModuleRules
+{
+	public EmergenceTests(ReadOnlyTargetRules Target) : base(Target)
+	{	
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		CppStandard = CppStandardVersion.Cpp17;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"FunctionalTesting",
+				"Emergence"
+			}
+		);
+		
+	}
+}
