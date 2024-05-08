f68babbd65eee2deb2a9d3894527705dde5e9769
Christopher Lee <minichris502@gmail.com>
2024-05-08 14:06:02 +0100

    updated function again

--- Plugins/Emergence/Source/Emergence/Private/Emergence.cpp
+++ Plugins/Emergence/Source/Emergence/Private/Emergence.cpp
@@ -30,7 +30,7 @@ void FEmergenceModule::StartupModule()
 	typedef int(*_AddFunction)(int a, int b);
 	_AddFunction AddFunction = (_AddFunction)FPlatformProcess::GetDllExport(ExampleLibraryHandle2, TEXT("aotsample_add"));
 
-	typedef char*(*_CombineStringFunction)(char* StringA, char* StringB);
+	typedef char*(*_CombineStringFunction)(const char* StringA, const char* StringB);
 	_CombineStringFunction CombineStringFunction = (_CombineStringFunction)FPlatformProcess::GetDllExport(ExampleLibraryHandle2, TEXT("aotsample_sumstring"));
 
 	int a = AddFunction(5, 12);
