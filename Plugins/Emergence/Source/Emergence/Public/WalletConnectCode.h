d4a6a4b3cb4f5bc26a5ed5bfe97661c5be0ca406
Christopher Lee <minichris502@gmail.com>
2024-06-17 16:28:30 +0100

    WalletConnectCode time set to 120 seconds

--- Plugins/Emergence/Source/Emergence/Public/WalletConnectCode.h
+++ Plugins/Emergence/Source/Emergence/Public/WalletConnectCode.h
@@ -62,7 +62,7 @@ public:
 
 	//How long before the QR code / WalletConnect URI changes. This can be modified if users are struggling to login in the time provided. It is important to allow the login attempt to reset eventually, just in case the login gets into a "stuck state" for whatever reason. Note: this should be changed before the login attempt starts. Changing after that will have no effect.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Time")
-	int ConnectionRefreshTime = 60;
+	int ConnectionRefreshTime = 120;
 
 	//This is the seconds remaining until the QR code is refreshed. Use this to display to the user how long they have.
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Time")
