ff848ca1f871c384af0165f1f862b8ef79973d3f
Christopher Lee <minichris502@gmail.com>
2024-09-02 15:07:03 +0100

    added script to build sample project

---
+++ SampleProjectBase/Source/EmergenceSample/EmergenceSample.Build.cs
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class EmergenceSample : ModuleRules
+{
+	public EmergenceSample(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Emergence" });
+	}
+}
--- WindowsDestroyFolders.bat
+++ WindowsDestroyFolders.bat
@@ -6,11 +6,11 @@ RMDIR %~dp0Intermediate /S /q
 for /d %%D in ("%~dp0\Plugins\*") do (
     REM Check if the specific sub-folder exists
     if exist "%%D\Binaries" (
-		del /s /f /q %%DBinaries\*.*
+		del /s /f /q %%D\Binaries\*.*
         rmdir /s /q "%%D\Binaries"
     )
 	if exist "%%D\Intermediate" (
-	del /s /f /q %%DIntermediate\*.*
+	del /s /f /q %%D\Intermediate\*.*
         rmdir /s /q "%%D\Intermediate"
     )
 )
