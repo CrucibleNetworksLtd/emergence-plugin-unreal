a9d83ece0af72ebfe613defb7f41b4e20d55683b
Christopher Lee <minichris502@gmail.com>
2025-01-14 12:07:54 +0000

    GetBalance is no longer marked EmergenceInternal

--- Plugins/Emergence/Source/Emergence/Public/WalletService/GetBalance.h
+++ Plugins/Emergence/Source/Emergence/Public/WalletService/GetBalance.h
@@ -20,7 +20,7 @@ public:
 	 * @param Address Address to get the balance of.
 	 * @param Blockchain Blockchain to get the balance on.
 	 */
-	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject"), Category = "Emergence Internal|Overlay Methods")
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject"), Category = "Emergence|Blockchain Interactions")
 	static UGetBalance* GetBalance(UObject* WorldContextObject, FString Address, UEmergenceChain* Blockchain);
 
 	virtual void Activate() override;
