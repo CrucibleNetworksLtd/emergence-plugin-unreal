8d92ab1d69f60e58415c364a5a68b6d086dbe5dc
Christopher Lee <minichris502@gmail.com>
2025-01-22 21:36:24 +0000

    Added GetAgents node

---
+++ Plugins/Eliza/Source/Eliza/Private/GetAgents.cpp
@@ -0,0 +1,63 @@
+// Copyright Crucible Networks Ltd 2025. All Rights Reserved.
+
+
+#include "GetAgents.h"
+#include "HttpService/ElizaHttpHelperLibrary.h"
+#include "Interfaces/IHttpResponse.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+
+UGetAgents* UGetAgents::GetAgents()
+{
+	UGetAgents* BlueprintNode = NewObject<UGetAgents>();
+	return BlueprintNode;
+}
+
+void UGetAgents::Activate()
+{
+	FString requestURL = UElizaHttpHelperLibrary::GetElizaStarterUrl() + "/Agents";
+	//TArray<TPair<FString, FString>> Headers;
+	//Headers.Add(TPair<FString, FString>{"content-type", "application/json"});
+
+	/*TSharedPtr<FJsonObject> BodyContentJsonObject = MakeShareable(new FJsonObject);
+	BodyContentJsonObject->SetStringField("text", Message);
+	FString JsonOutput;
+	TSharedRef< TJsonWriter<> > Writer = TJsonWriterFactory<>::Create(&JsonOutput);
+	FJsonSerializer::Serialize(BodyContentJsonObject.ToSharedRef(), Writer);
+	*/
+	UElizaHttpHelperLibrary::ExecuteHttpRequest<UGetAgents>(
+		this,
+		&UGetAgents::GetAgents_HttpRequestComplete,
+		requestURL/*,
+		"GET",
+		60.0F,
+		Headers,
+		JsonOutput*/
+	);
+}
+
+void UGetAgents::GetAgents_HttpRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	// [{"user":"eliza","text":"Loud and clear! My circuits are humming along, ready for whatever test you throw my way. If you need a debugging buddy or just someone to share the testing woes with, I'm here. How's everything looking on your end?","action":"NONE"}]
+	if (bSucceeded) {
+		TSharedPtr<FJsonValue> JsonValue;
+		FString ResponseString = HttpResponse->GetContentAsString();
+		UE_LOG(LogEliza, Display, TEXT("Message Agent response: %s"), *ResponseString);
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ResponseString);
+		if (FJsonSerializer::Deserialize(Reader, JsonValue)) {
+			TArray<FAgentInfo> Agents;
+			auto AgentsArray = JsonValue->AsObject()->GetArrayField("agents");
+			for (int i = 0; i < AgentsArray.Num(); i++) {
+				auto Agent = AgentsArray[i]->AsObject();
+				Agents.Add(FAgentInfo(Agent->GetStringField("id"), Agent->GetStringField("name")));
+			}
+			OnGetAgentsCompleted.Broadcast(true, Agents);
+
+			return;
+		}
+	}
+
+	OnGetAgentsCompleted.Broadcast(false, TArray<FAgentInfo>());
+	return;
+}
---
+++ Plugins/Eliza/Source/Eliza/Public/GetAgents.h
@@ -0,0 +1,52 @@
+// Copyright Crucible Networks Ltd 2025. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "Interfaces/IHttpRequest.h"
+#include "GetAgents.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAgentInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Agent")
+	FString AgentId;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Agent")
+	FString Name;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Agent")
+	//TArray<FString> Clients;
+
+	FAgentInfo() {};
+
+	FAgentInfo(FString _AgentId, FString _Name/*, TArray<FString> _Clients*/) {
+		AgentId = _AgentId;
+		Name = _Name;
+		//Clients = _Clients;
+	};
+};
+
+UCLASS()
+class ELIZA_API UGetAgents : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "Eliza")
+	static UGetAgents* GetAgents();
+
+	virtual void Activate() override;
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnGetAgentsCompleted, bool, Success, const TArray<FAgentInfo>&, Agents);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGetAgentsCompleted OnGetAgentsCompleted;
+private:
+	void GetAgents_HttpRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded);
+
+	FString AgentId, Message;
+};
