50441da49b1c2b659526d475c9e4d97ef0f3b100
Christopher Lee <minichris502@gmail.com>
2025-01-27 12:59:31 +0000

    Attempted to add agent speak

---
+++ Plugins/Eliza/Source/Eliza/Private/AgentSpeak.cpp
@@ -0,0 +1,63 @@
+// Copyright Crucible Networks Ltd 2025. All Rights Reserved.
+
+
+#include "AgentSpeak.h"
+#include "HttpService/ElizaHttpHelperLibrary.h"
+#include "Interfaces/IHttpResponse.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+
+UAgentSpeak* UAgentSpeak::AgentSpeak(FString _AgentId, FString _Message)
+{
+	UAgentSpeak* BlueprintNode = NewObject<UAgentSpeak>();
+	BlueprintNode->AgentId = _AgentId;
+	BlueprintNode->Message = _Message;
+	return BlueprintNode;
+}
+
+void UAgentSpeak::Activate()
+{
+	FString requestURL = UElizaHttpHelperLibrary::GetElizaStarterUrl() + "/" + AgentId + "/speak";
+	TArray<TPair<FString, FString>> Headers;
+	Headers.Add(TPair<FString, FString>{"content-type", "application/json"});
+
+	TSharedPtr<FJsonObject> BodyContentJsonObject = MakeShareable(new FJsonObject);
+	BodyContentJsonObject->SetStringField("text", Message);
+	FString JsonOutput;
+	TSharedRef< TJsonWriter<> > Writer = TJsonWriterFactory<>::Create(&JsonOutput);
+	FJsonSerializer::Serialize(BodyContentJsonObject.ToSharedRef(), Writer);
+
+	UElizaHttpHelperLibrary::ExecuteHttpRequest<UAgentSpeak>(
+		this,
+		&UAgentSpeak::AgentSpeak_HttpRequestComplete,
+		requestURL,
+		"POST",
+		60.0F,
+		Headers,
+		JsonOutput
+	);
+}
+
+void UAgentSpeak::AgentSpeak_HttpRequestComplete(FHttpRequestPtr HttpRequest, FHttpResponsePtr HttpResponse, bool bSucceeded)
+{
+	// [{"user":"eliza","text":"Loud and clear! My circuits are humming along, ready for whatever test you throw my way. If you need a debugging buddy or just someone to share the testing woes with, I'm here. How's everything looking on your end?","action":"NONE"}]
+	if (bSucceeded) {
+		TSharedPtr<FJsonValue> JsonValue;
+		FString ResponseString = HttpResponse->GetContentAsString();
+		UE_LOG(LogEliza, Display, TEXT("Message Agent response: %s"), *ResponseString);
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ResponseString);
+		if (FJsonSerializer::Deserialize(Reader, JsonValue)) {
+			TSharedPtr<FJsonValue> MessageData = JsonValue->AsArray()[0];
+			FString User = MessageData->AsObject()->GetStringField("user");
+			FString Text = MessageData->AsObject()->GetStringField("text");
+			FString Action = MessageData->AsObject()->GetStringField("action");
+
+			OnAgentSpeakCompleted.Broadcast(true, User, Text, Action);
+			return;
+		}
+	}
+
+	OnAgentSpeakCompleted.Broadcast(false, FString(), FString(), FString());
+	return;
+}
